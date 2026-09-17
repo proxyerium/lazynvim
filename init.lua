fe8a4cbd19915481d11ccd647294528444bd0a9b
proxyerium <proxyerium@outlook.com>
2026-09-17 23:46:51 +0800

    <Tab> to confirm completion

--- init.lua
+++ init.lua
@@ -64,7 +64,7 @@ require("lazy").setup({
 						["<C-f>"] = cmp.mapping.scroll_docs(4),
 						["<C-Space>"] = cmp.mapping.complete(),
 						["<C-e>"] = cmp.mapping.abort(),
-						["<CR>"] = cmp.mapping.confirm({ select = true }),
+						["<Tab>"] = cmp.mapping.confirm({ select = true }),
 					}),
 					sources = cmp.config.sources({
 						{ name = "nvim_lsp" }, -- LSP 补全
